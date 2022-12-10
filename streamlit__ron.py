4beeb13782e2d761508f262de77abb6ad67ab189
Rontzur322 <118887856+Rontzur322@users.noreply.github.com>
2022-12-10 15:50:40 +0200

    Update streamlit__ron.py

--- streamlit__ron.py
+++ streamlit__ron.py
@@ -12,11 +12,16 @@ if uploaded_file is not None:
     # Read the image file
     image = cv2.imread(file_path)
 
-    # Convert the image to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Check if the image was successfully read
+    if image is not None:
+        # Convert the image to grayscale
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # Apply the Canny edge detector to the grayscale image
-    edges = cv2.Canny(gray, 50, 100)
+        # Apply the Canny edge detector to the grayscale image
+        edges = cv2.Canny(gray, 50, 100)
 
-    # Display the original image and the edge-detected image
-    st.image([image, edges], caption=["Original Image", "Edge-detected Image"])
+        # Display the original image and the edge-detected image
+        st.image([image, edges], caption=["Original Image", "Edge-detected Image"])
+    else:
+        # Display an error message if the image could not be read
+        st.error("Could not read image file")
