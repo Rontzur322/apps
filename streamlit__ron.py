934c5da1618d0e2af5f3aa0997cb257fa316e992
Rontzur322 <118887856+Rontzur322@users.noreply.github.com>
2022-12-10 15:38:01 +0200

    Update streamlit__ron.py

--- streamlit__ron.py
+++ streamlit__ron.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
+# Create a sidebar
+st.sidebar.title("Sidebar")
+
+# Add some options to the sidebar
+st.sidebar.checkbox("Option 1")
+st.sidebar.checkbox("Option 2")
+st.sidebar.checkbox("Option 3")
+
 # Ask the user to upload an image file
 uploaded_file = st.file_uploader("Choose an image file")
 
