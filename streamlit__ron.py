4983c00b3f9d30004730b110888f551cde280136
Rontzur322 <118887856+Rontzur322@users.noreply.github.com>
2022-12-10 15:30:47 +0200

    Update streamlit__ron.py

--- streamlit__ron.py
+++ streamlit__ron.py
@@ -1,8 +1,25 @@
 import streamlit as st
+import cv2
 
-# Allow user to upload image
-uploaded_file = st.file_uploader("Choose an image file:")
+# Ask the user to upload an image file
+uploaded_file = st.file_uploader("Choose an image file")
 
-# Show the uploaded image
+# Check if the user has uploaded a file
 if uploaded_file is not None:
-    st.image(uploaded_file, caption="Uploaded image", use_column_width=True)
+    # Read the image file
+    image = cv2.imread(uploaded_file)
+
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Create a sidebar
+    st.sidebar.title("Canny Edge Detector")
+
+    # Add a slider to the sidebar for controlling the edge detection threshold
+    threshold = st.sidebar.slider("Threshold", 0, 255, 50)
+
+    # Apply the Canny edge detector to the grayscale image
+    edges = cv2.Canny(gray, threshold, threshold * 2)
+
+    # Display the original image and the edge-detected image
+    st.image([image, edges], caption=["Original Image", "Edge-detected Image"])
