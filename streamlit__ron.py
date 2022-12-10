632346a0dd0f417ed90a508668a59db0b99b2dfc
Rontzur322 <118887856+Rontzur322@users.noreply.github.com>
2022-12-10 16:03:06 +0200

    Update streamlit__ron.py

--- streamlit__ron.py
+++ streamlit__ron.py
@@ -1,30 +1,9 @@
 import streamlit as st
-import cv2
 
 # Ask the user to upload an image file
 uploaded_file = st.file_uploader("Choose an image file")
 
 # Check if the user has uploaded a file
 if uploaded_file is not None:
-    # Get the path to the uploaded file
-    file_path = uploaded_file.path
-
-    # Debug the file path
-    print(file_path)
-
-    # Read the image file
-    image = cv2.imread(file_path)
-
-    # Check if the image was successfully read
-    if image is not None:
-        # Convert the image to grayscale
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        # Apply the Canny edge detector to the grayscale image
-        edges = cv2.Canny(gray, 50, 100)
-
-        # Display only the edge-detected image
-        st.image(edges, caption="Edge-detected Image")
-    else:
-        # Display an error message if the image could not be read
-        st.error("Could not read image file")
+    # Display the uploaded image
+    st.image(uploaded_file)
